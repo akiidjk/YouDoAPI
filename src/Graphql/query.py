93691aefff522ac133cd75f71cdb931071a69ad3
Retr0 <dragonfaiver@gmail.com>
2024-02-08 17:26:29 +0100

    feat: Add Pomodoro queries to GraphQL API
    - Added new queries to retrieve Pom data from the GraphQL API.
    - Added `get_all_pomodoro`, `get_by_id_pomodoro`, `get_by_user_pomodoro`, and `get_by_month_pomodoro` queries to the `Query` class.
    - Updated imports in the `query.py` file to include `PomodoroType` and `PomodoroService`.
    - The new queries allow users to retrieve Pomodoro data based on different parameters such as user ID and month.
    - This commit will trigger a release bumping a MINOR version.

--- src/Graphql/query.py
+++ src/Graphql/query.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+from uuid import UUID
 import strawberry
 
-from schema import TodoType, UserType
+from schema import PomodoroType, TodoType, UserType
 from service.dailyTodo import TodoService
+from service.pomodoro import PomodoroService
 from service.user import UserService
 
 
@@ -12,6 +15,9 @@ class Query:
     Returns:
         _type_: _description_
     """
+
+#* Todo Query
+
     @strawberry.field
     async def get_all_todo(self) -> list[TodoType]:
         """Get all todo data.
@@ -22,7 +28,7 @@ class Query:
         return await TodoService.get_all()
 
     @strawberry.field
-    async def get_by_user_todo(self,user_id:int) -> list[TodoType]:
+    async def get_by_user_todo(self,user_id:UUID) -> list[TodoType]:
         """Get one todo data by email.
 
         Args:
@@ -34,7 +40,7 @@ class Query:
         return await TodoService.get_by_user(user_id)
 
     @strawberry.field
-    async def get_by_id_todo(self,id:int) -> TodoType:
+    async def get_by_id_todo(self,id:UUID) -> TodoType:
         """Get one todo data by id.
 
         Args:
@@ -44,6 +50,8 @@ class Query:
             DailyTodoType: _description_
         """
         return await TodoService.get_by_id(id)
+
+#* User Query
 
     @strawberry.field
     async def get_all_user(self) -> list[UserType]:
@@ -55,7 +63,7 @@ class Query:
         return await UserService.get_all()
 
     @strawberry.field
-    async def get_by_id_user(self,id:int) -> UserType:
+    async def get_by_id_user(self,id:UUID) -> UserType:
         """Get one user data by id.
 
         Args:
@@ -77,3 +85,51 @@ class Query:
             DailyTodoType: _description_
         """
         return await UserService.get_by_email(email)
+
+#* Pomodoro Query
+
+    @strawberry.field
+    async def get_all_pomodoro(self) -> list[PomodoroType]:
+        """Get all user data.
+
+        Returns:
+            list[userType]: _description_
+        """
+        return await PomodoroService.get_all()
+
+    @strawberry.field
+    async def get_by_id_pomodoro(self,id:UUID) -> PomodoroType:
+        """Get one user data by id.
+
+        Args:
+            id (int): _description_
+
+        Returns:
+            DailyTodoType: _description_
+        """
+        return await PomodoroService.get_by_id(id)
+
+    @strawberry.field
+    async def get_by_user_pomodoro(self,user_id:UUID) -> list[PomodoroType]:
+        """Get one todo data by email.
+
+        Args:
+            user_id (int): The email of user
+
+        Returns:
+            DailyTodoType: _description_
+        """
+        return await PomodoroService.get_by_user(user_id)
+
+    @strawberry.field
+    async def get_by_month_pomodoro(self,user_id:UUID,date:datetime) -> list[PomodoroType]:
+        """Get one todo data by email.
+
+        Args:
+            user_id (int): The email of user
+            date (datetime): The date
+
+        Returns:
+            DailyTodoType: _description_
+        """
+        return await PomodoroService.get_by_month(user_id,date)
