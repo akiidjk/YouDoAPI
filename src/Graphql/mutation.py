65941e93afe48b18007f5d05ef7aa1e6d797ad75
Retr0 <dragonfaiver@gmail.com>
2024-02-08 17:26:23 +0100

    feat: Add pomodoro mutation to create, delete, and pomodoro tasks

--- src/Graphql/mutation.py
+++ src/Graphql/mutation.py
@@ -1,7 +1,9 @@
+from uuid import UUID
 import strawberry
 
-from schema import TodoInput, TodoType, UserInput, UserType
+from schema import PomodoroInput, PomodoroType, TodoInput, TodoType, UserInput, UserType
 from service.dailyTodo import TodoService
+from service.pomodoro import PomodoroService
 from service.user import UserService
 
 @strawberry.type
@@ -11,6 +13,8 @@ class Mutation:
     Returns:
         _type_: _description_
     """
+
+#* Todo mutation
 
     @strawberry.mutation
     async def create_todo(self,todo_data:TodoInput) -> TodoType:
@@ -25,7 +29,7 @@ class Mutation:
         return await TodoService.create(todo_data=todo_data)
 
     @strawberry.mutation
-    async def delete_todo(self,id:int) -> int:
+    async def delete_todo(self,id:UUID) -> UUID:
         """Delete an existing todo case by its id.
 
         Args:
@@ -37,7 +41,7 @@ class Mutation:
         return await TodoService.delete(id)
 
     @strawberry.mutation
-    async def update_todo(self,id:int,todo_data:TodoInput) -> str:
+    async def update_todo(self,id:UUID,todo_data:TodoInput) -> str:
         """Update an existing todo case's information.
 
         Args:
@@ -48,6 +52,8 @@ class Mutation:
             str: _description_
         """
         return await TodoService.update(id,todo_data)
+
+#* User mutation
 
     @strawberry.mutation
     async def create_user(self,user_data:UserInput) -> UserType:
@@ -62,7 +68,7 @@ class Mutation:
         return await UserService.create(user_data=user_data)
 
     @strawberry.mutation
-    async def delete_user(self,id:int) -> int:
+    async def delete_user(self,id:UUID) -> UUID:
         """Delete an existing user case by its id.
 
         Args:
@@ -74,7 +80,7 @@ class Mutation:
         return await UserService.delete(id)
 
     @strawberry.mutation
-    async def update_user(self,id:int,user_data:UserInput) -> str:
+    async def update_user(self,id:UUID,user_data:UserInput) -> str:
         """Update an existing user case's information.
 
         Args:
@@ -86,3 +92,41 @@ class Mutation:
         """
         return await UserService.update(id,user_data)
 
+#* Pomodoro mutation
+
+    @strawberry.mutation
+    async def create_pomodoro(self,pomodoro_data:PomodoroInput) -> PomodoroType:
+        """Create a new user case with given data.
+
+        Args:
+            pomodoro_data (PomodoroInput): _description_
+
+        Returns:
+            userType: _description_
+        """
+        return await PomodoroService.create(pomodoro_data=pomodoro_data)
+
+    @strawberry.mutation
+    async def delete_pomodoro(self,id:UUID) -> int:
+        """Delete an existing user case by its id.
+
+        Args:
+            id (int): _description_
+
+        Returns:
+            str: _description_
+        """
+        return await PomodoroService.delete(id)
+
+    @strawberry.mutation
+    async def update_pomodoro(self,id:int,pomodoro_data:PomodoroInput) -> str:
+        """Update an existing user case's information.
+
+        Args:
+            id (int): _description_
+            pomodoro_data (PomodoroInput): _description_
+
+        Returns:
+            str: _description_
+        """
+        return await PomodoroService.update(id,pomodoro_data=pomodoro_data)
--- src/schema.py
+++ src/schema.py
@@ -3,28 +3,38 @@
 #! The file declare the two type for the GraphQL quey #
 #######################################################
 
+from datetime import datetime
+
+from uuid import UUID
 import strawberry
 
 # * When get a row
 @strawberry.type
 class TodoType:
     """Class to represent the data of todoDaily in the graphql query."""
-    id:int
+    id: UUID
     title:str
     description:str
-    date_created:str
-    date_expire:str
+    datetime_created:datetime
+    datetime_expire:datetime
     status_done:bool
-    user_id:int
+    user_id:UUID
     priority:int
     favorite:bool
 
 @strawberry.type
 class UserType:
     """Class to represent the data of User in the graphql query."""
-    id:int
+    id: UUID
     email:str
 
+@strawberry.type
+class PomodoroType:
+    """Class to represent the data of User in the graphql query."""
+    id: UUID
+    date_time:datetime
+    duration:int
+    user_id: UUID
 
 # * When put a element
 @strawberry.input
@@ -32,10 +42,10 @@ class TodoInput:
     """Class to represent the data in the mutation query."""
     title:str
     description:str
-    date_created:str
-    date_expire:str
+    datetime_created:datetime
+    datetime_expire:datetime
     status_done:bool
-    user_id:int
+    user_id:UUID
     priority:int
     favorite:bool
 
@@ -43,5 +53,12 @@ class TodoInput:
 class UserInput:
     """Class to represent the data in the mutation query."""
     email:str
+
+@strawberry.input
+class PomodoroInput:
+    """Class to represent the data in the mutation query."""
+    date_time:datetime
+    duration:int
+    user_id:UUID
 
 
